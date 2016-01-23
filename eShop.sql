504cf4fc7fba7b54a6ccba102e4f53e5258914ad
SalmaNegm <salma-negm@outlook.com>
2016-01-23 20:24:02 +0200

    remove cID column from products table

--- eShop.sql
+++ eShop.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 22, 2016 at 04:04 PM
+-- Generation Time: Jan 23, 2016 at 08:22 PM
 -- Server version: 5.5.46-0ubuntu0.14.04.2
 -- PHP Version: 5.5.9-1ubuntu4.14
 
@@ -109,10 +109,8 @@ CREATE TABLE IF NOT EXISTS `products` (
   `pQuantity` int(11) NOT NULL,
   `pImg` varchar(50) NOT NULL,
   `pDesc` varchar(100) NOT NULL,
-  `cID` int(11) unsigned NOT NULL,
   `scID` int(11) unsigned NOT NULL,
   PRIMARY KEY (`pID`),
-  KEY `cID` (`cID`),
   KEY `scID` (`scID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='products info and categories' AUTO_INCREMENT=1 ;
 
@@ -145,8 +143,8 @@ ALTER TABLE `customerInterests`
 -- Constraints for table `orderItems`
 --
 ALTER TABLE `orderItems`
-  ADD CONSTRAINT `orderItems_ibfk_2` FOREIGN KEY (`pID`) REFERENCES `products` (`pID`),
-  ADD CONSTRAINT `orderItems_ibfk_1` FOREIGN KEY (`oID`) REFERENCES `orders` (`oID`);
+  ADD CONSTRAINT `orderItems_ibfk_1` FOREIGN KEY (`oID`) REFERENCES `orders` (`oID`),
+  ADD CONSTRAINT `orderItems_ibfk_2` FOREIGN KEY (`pID`) REFERENCES `products` (`pID`);
 
 --
 -- Constraints for table `orders`
@@ -158,8 +156,7 @@ ALTER TABLE `orders`
 -- Constraints for table `products`
 --
 ALTER TABLE `products`
-  ADD CONSTRAINT `products_ibfk_2` FOREIGN KEY (`scID`) REFERENCES `subcategories` (`scID`),
-  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`cID`) REFERENCES `categories` (`cID`);
+  ADD CONSTRAINT `products_ibfk_2` FOREIGN KEY (`scID`) REFERENCES `subcategories` (`scID`);
 
 --
 -- Constraints for table `subcategories`
