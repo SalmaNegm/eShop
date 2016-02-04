6646dea18ceeb8e545a4f1db80ff16472c4ef096
SalmaNegm <salma-negm@outlook.com>
2016-02-04 23:54:10 +0200

    deleted unwanted files

--- pages/home/signedCustomer_nav.php
+++
@@ -1,9 +0,0 @@
-<div id="menu_tab">
-      <ul class="menu">
-        <li><a href="home.php" class="nav"> Home </a></li>
-        <li class="divider"></li>
-        <li><a href="#" class="nav">My account</a></li>
-        <li class="divider"></li>
-        <li><a href="#" class="nav">logout</a></li>
-      </ul>
-    </div>
