03dfb1f9f12de2f87e58d76fbd26b5f8f508048b
Fatmasarhan <fatmasar7an@gmail.com>
2016-02-05 22:59:47 +0200

    slider

--- pages/admin/home.php
+++ pages/admin/home.php
@@ -7,6 +7,8 @@ include '../../classes/products.php';
   <head>
     <title>Baby Shop</title>
     <meta http-equiv="Content-Type" content="text/html; charset=windows-1252" />
+
+   <link rel="stylesheet" href="../../try.css" type="text/css" media="screen" />
     <script type="text/javascript" src="../../jquery-1.12.0.min.js"></script>
     <script type="text/javascript" src="../../js/add.js"></script>
     <script src="../../js/bootstrap.min.js"></script>
@@ -36,8 +38,8 @@ include '../../classes/products.php';
         ?>
 
         <div class="container"> 
-           <span class="glyphicon glyphicon-home"></span>
-           <span class="current">Home</span>    
+           <span class="glyphicon glyphicon-home icon"></span>
+           <span class="current icon" >Home</span>    
         </div>
 
         <div class="left_content">
@@ -51,6 +53,58 @@ include '../../classes/products.php';
       
       <div class="center_content">
       <div class="center_title_bar">Latest Products</div>
+
+      <!-- *********************************** slider************************-->
+      <div id="myCarousel" class="carousel slide" data-ride="carousel" >
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+
+      <div class="item active">
+
+        <img src="../../images/9.png" alt="Chania" >
+        <div class="carousel-caption">
+          <h3>Travel</h3>
+         
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="../../images/10.jpg" alt="Chania" >
+        <div class="carousel-caption">
+          <h3>SleepinG</h3>
+          
+        </div>
+      </div>
+    
+      <div class="item">
+        <img src="../../images/11.jpg" alt="Flower" >
+        <div class="carousel-caption">
+          <h3>Safety</h3>
+          
+        </div>
+      </div>
+  
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+<!--******************************************************************************-->
+    <br/><br/>
       <?php 
       $data = $prod -> last_products() ;
       foreach ($data as $key => $row) 
@@ -61,7 +115,7 @@ include '../../classes/products.php';
           $price = $row["pPrice"];
           $img = $row["pImg"];
           $id= $row["pID"];
-        }
+        } 
         echo "<div class='prod_box'>";
         echo " <div class='center_prod_box'>";
         echo "<div class='product_title'><a href='#'>$name</a></div>";
@@ -70,7 +124,9 @@ include '../../classes/products.php';
         echo " </div>";
         echo "<div class='prod_details_tab'> <a href='cart.php?id=$id' class='prod_buy'>Add to Cart</a> <a href='details.php?id=$id' class='prod_details'>Details</a> </div>";
         echo "</div>";
-      }
+      
+       }
+
       ?>
 
     </div> <!-- end of center content -->
